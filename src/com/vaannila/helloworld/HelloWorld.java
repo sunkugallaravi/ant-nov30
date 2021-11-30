5c6785f6283aaab62fada974d98b6c0a02c82172
root <root@jenkins1.asia-south2-a.c.jenkinspractice-328803.internal>
2021-11-30 18:48:22 +0000

    ant code added

---
+++ src/com/vaannila/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.vaannila.helloworld;
+public class HelloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello Satya welcome to GCP Devops!");
+    }
+}
+
+
